817a9249c373a8ec33d1897dce2e895e6950be9f
Sesar Hersisson <sesarh@icloud.com>
2019-11-26 14:04:30 +0000

    lagaði stafsetnigavillu

--- src/lib/list.js
+++ src/lib/list.js
@@ -21,11 +21,7 @@ export default class List {
       for(var i = 0; i < lectures.length; i += 1) {
         if(lectures[i].category === type || !type) {
           const lecture = createListElement(lectures[i].thumbnail, lectures[i].title, lectures[i].category);
-<<<<<<< HEAD
           lecture.addEventListener('click', loadLecture(i));
-=======
-          //lecture.addEventlistener('click', loadLecture(i));
->>>>>>> 866fed889d805e0488cfa0df8b1be8d91e06c999
           this.container.appendChild(lecture);
         }
       }
