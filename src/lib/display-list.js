d63e260153f9697fcbf485c1a8186439002e85ac
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2019-11-26 22:04:52 +0000

    öll rétt gögn birtast á fyrirlestra síðu eftir að gera útlit

--- src/lib/display-list.js
+++ src/lib/display-list.js
@@ -5,35 +5,34 @@ let display = Array();
 let list;
 
 export default function init(pageParam) {
-    list = new List();
-    list.load();
-    page = pageParam;
-    const buttons = page.querySelectorAll('.button--fyrirlestrar');
-    for(var i = 0; i < buttons.length; i += 1) {
-        buttons[i].addEventListener('click', toggleList);
-    }
+  list = new List();
+  list.load();
+  page = pageParam;
+  const buttons = page.querySelectorAll('.button--fyrirlestrar');
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', toggleList);
+  }
 }
 
 function toggleList(e) {
-    e.preventDefault();
-    let button = e.toElement;
-    const category = button.firstChild.nodeValue.toLowerCase();
-    if(display.includes(category)) {
-        button.classList.remove('button--fyrirlestrar--active');
-        var index = display.indexOf(category);
-        display.splice(index, 1);
-        list.load(display);
-    } else {
-        button.classList.add('button--fyrirlestrar--active');
-        display.push(category);
-        if(display.length === 3) {
-            const buttons = page.querySelectorAll('.button--fyrirlestrar');
-            for(var i = 0; i < buttons.length; i += 1) {
-                buttons[i].classList.remove('button--fyrirlestrar--active');
-            }
-            display = Array();
-        }
-        list.load(display);
+  e.preventDefault();
+  const button = e.toElement;
+  const category = button.firstChild.nodeValue.toLowerCase();
+  if (display.includes(category)) {
+    button.classList.remove('button--fyrirlestrar--active');
+    const index = display.indexOf(category);
+    display.splice(index, 1);
+    list.load(display);
+  } else {
+    button.classList.add('button--fyrirlestrar--active');
+    display.push(category);
+    if (display.length === 3) {
+      const buttons = page.querySelectorAll('.button--fyrirlestrar');
+      for (let i = 0; i < buttons.length; i += 1) {
+        buttons[i].classList.remove('button--fyrirlestrar--active');
+      }
+      display = Array();
     }
-    
+    list.load(display);
+  }
 }
--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -1,14 +1,68 @@
-export default function createLecture(efni) {
+import { el } from './helpers';
+
+/**
+ *
+ * @param {Array} content
+ */
+function createElement(content) {
+  let element;
+  if (content['type'] === 'youtube') {
+    element = el('iframe', 'video');
+    element.src = content['data'];
+    element.setAttribute('width', '100%');
+    element.setAttribute('height', '100%');
+  } else if (content['type'] === 'text') {
+    element = el('p', 'text');
+    element.innerHTML = content['data'];
+  } else if (content['type'] === 'quote') {
+    element = el('div', 'quote', el('blockquote', 'quote__data'), el('p', 'quote__attribute'));
+    element.querySelector('.quote__data').innerHTML = content['data'];
+    if (content['attribute']) {
+      element.querySelector('.quote__attribute').innerHTML = content['attribute'];
+    }
+  } else if (content['type'] === 'image') {
+    element = el('div', 'image', el('img', 'image__img'), el('p', 'image__caption'));
+    element.querySelector('.image__img').src = content['data'];
+    if (content['caption']) {
+      element.querySelector('.image__caption').innerHTML = content['caption'];
+    }
+  } else if (content['type'] === 'heading') {
+    element = el('h2', 'haus');
+    element.innerHTML = content['data'];
+  } else if (content['type'] === 'list') {
+    element = el('ul', 'list');
+    for (let i = 0; i < content['data'].length; i += 1) {
+      const e = el('li', 'list__tag');
+      e.innerHTML = content['data'][i];
+      element.appendChild(e);
+    }
+  } else if (content['type'] === 'code') {
+    element = el('code', 'code');
+    element.innerHTML = content['data'];
+  }
+  const col = el('div', 'content__col');
+  col.appendChild(element);
+  return col;
+}
+
+export default function createLecture(fyrirlestur) {
   const haus = document.querySelector('.header');
-  if (efni.image) {
-    haus.style.backgroundImage = `url(${efni.image})`;
+  if (fyrirlestur.image) {
+    haus.style.backgroundImage = `url(${fyrirlestur.image})`;
   } else {
     haus.style.backgroundImage = 'none';
     haus.style.backgroundColor = '#ccc';
   }
-  haus.querySelector('.header__subtitle').innerHTML = efni.category;
-  haus.querySelector('.header__title').innerHTML = efni.title;
-  /*for(let i = 0; i < efni.length; i += 1) {
-    const el = element(efni[i]);
-  }*/
+  haus.querySelector('.header__subtitle').innerHTML = fyrirlestur.category;
+  haus.querySelector('.header__title').innerHTML = fyrirlestur.title;
+  // haus klár
+  const efni = fyrirlestur.content;
+  // meginmál
+  const row = el('div', 'content__row');
+  for (let i = 0; i < efni.length; i += 1) {
+    const e = createElement(efni[i]);
+    row.appendChild(e);
+  }
+  const content = el('div', 'content', row);
+  document.querySelector('.main').appendChild(content);
 }
