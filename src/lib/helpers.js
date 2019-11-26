67bfee0822732c701afe13f25200155b378dfa4c
Sesar Hersisson <sesarh@icloud.com>
2019-11-26 13:12:15 +0000

    Vinnsla í birtingu lista

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -4,9 +4,11 @@ export default function empty(element) {
   }
 }
 
-export function el(name, ...children) {
+export function el(name, className, ...children) {
   const element = document.createElement(name);
-
+  if (className) {
+    element.classList.add(className);
+  }
   if (Array.isArray(children)) {
     children.forEach((child) => {
       if (typeof child === 'string') {
@@ -19,3 +21,35 @@ export function el(name, ...children) {
 
   return element;
 }
+
+ /**
+   * @returns {Promise}
+   */
+  export async function getLectures() {
+    return fetch('../../lectures.json')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error('Villa við að sækja gögn');
+    });
+  }
+
+  /**
+   * 
+   * @param {string} img 
+   * @param {string} title 
+   * @param {string} category 
+   */
+  export function createListElement(img, title, category) {
+    const listEl = el('div', 'lectures__col', el('div', 'lecture', el('img', 'lecture__img'), el('div', 'lecture__header', el('div', 'lecture__text', el('div', 'lecture__text__category'), el('div', 'lecture__text__title'), el('div', 'lecture__checked--done')))));
+    const imgEl = listEl.querySelector('.lecture__img');
+    imgEl.src = img;
+    const catEl = listEl.el('.lecture__text__category');
+    catEl.appendChild(document.createTextNode(category));
+    const titleEl = listEl.el('.lecture__text__title');
+    titleEl.appendChild(document.createTextNode(title));
+    
+    return listEl;
+  }
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,31 @@
-import empty from './helpers';
+import empty,{getLectures, createListElement} from './helpers';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    
   }
 
-  load() {
-    empty(this.container);
+  /**
+   * 
+   * @param {string} type
+   * Birtir lectures af flokknum type.
+   * Ef type er empty loadast allir lectures. 
+   */
+  load(type) {
+    //Listi yfir öll lecture elements
+    getLectures().then((response) => {
+      const lectures = response;
+      //Leitum yfir öll lecture element og birtum þau sem beðið er um
+      for(var i = 0; i < lectures.length; i += 1) {
+        if(lectures[i].category === type) {
+          const lecture = createListElement(lectures[i].image, lectures[i].title, lectures[i].category);
+          this.container.appendChild(lecture);
+        }
+      }
+    }).catch(()=>{}); 
+
   }
+
 }
+
