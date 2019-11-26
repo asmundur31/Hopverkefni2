bb8d3ec80f44c960b7477925249199494a35f337
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2019-11-26 14:05:40 +0000

    commit

--- src/lib/display-lecture.js
+++ src/lib/display-lecture.js
@@ -1,10 +1,12 @@
 import { getLectures } from './helpers';
 
 /**
- * 
- * @param {int} i 
+ *
+ * @param {int} i
  */
-function loadLecture(i){
+function loadLecture(i) {
+  debugger;
+  console.log(i);
   const data = getLectures() || [];
   const lecture = data[i];
 }
